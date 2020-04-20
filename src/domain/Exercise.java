ba3c541ccb739e6706b7412b40aeb0335b43e242
Ilya <mokienkoilya@gmail.com>
2020-04-20 19:37:08 +0300

    Палиндром
    Вроде  бы сделал всё кроме README потому, что не могу открыть ваш проект в Идее.

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,16 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    /**
+     * a function that takes a string, reverse it and compares with the original
+     * @param str contains a word to be checked for palindromy
+     * @return true if word is palindrome and false if not
+     */
+    public static boolean Calculate(String str) {
+        if(str.equals(new StringBuffer(str).reverse().toString()))
+        {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -2,11 +2,25 @@ package test;
 
 import domain.Exercise;
 
+import java.util.Scanner;
+
 public class TestResult {
-
     public static void main(String[] args) {
+        int key;
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите строку для проверки на палиндромность: ");
+            String str = in.nextLine();
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+            System.out.println(Exercise.Calculate(str));
+            if (Exercise.Calculate(str) == true) {
+                System.out.println("Это палендром");
+            } else {
+                System.out.println("Это не палендром");
+            }
+
+            System.out.println("1 - выход, 0 - продолжить");
+            key = in.nextInt();
+        }while (key!=1);
     }
 }
